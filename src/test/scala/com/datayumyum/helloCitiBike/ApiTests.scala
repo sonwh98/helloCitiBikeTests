d03aa9bd4377c6f5e7d2681444215126f0d7865d
sto <Son.C.To@gmail.com>
2014-04-24 01:32:05 -0400

    use json4s-native for parsing json

--- src/test/scala/com/datayumyum/helloCitiBike/ApiTests.scala
+++ src/test/scala/com/datayumyum/helloCitiBike/ApiTests.scala
@@ -1,62 +1,20 @@
 package com.datayumyum.helloCitiBike
 
-import argonaut._, Argonaut._
-import scalaz._, Scalaz._
-
-
 import org.junit.Test
+import org.json4s._
+import org.json4s.native.JsonMethods._
 
 class ApiTests {
-  
+
   @Test
   def json() {
     val jsonStr = scala.io.Source.fromURL("http://appservices.citibikenyc.com/data2/stations.php").getLines.mkString("\n")
-    println(jsonStr)
-    
+    val parsedResult = parse(jsonStr)
+    val stationIds = for {JArray(stations) <- parsedResult \\ "results"
+                   JObject(station) <- stations
+                   JField("id", JInt(id)) <- station
+    } yield id
+    println(stationIds)
   }
 
-  @Test
-  def quickstart() {
-    val input = """
-    [
-      { "name": "Mark", "age": 191 },
-    { "name": "Fred", "age": 33, "greeting": "hey ho, lets go!" },
-    { "name": "Barney", "age": 35, "address": {
-      "street": "rock street", "number": 10, "post_code": 2039
-    }}
-    ]
-    """
-
-    // parse the string as json, attempt to decode it to a list of person,
-    // otherwise just take it as an empty list.
-    val people = input.decodeOption[List[Person]].getOrElse(Nil)
-
-    // work with your data types as you normally would
-    val nice = people.map(person =>
-      person.copy(greeting = person.greeting.orElse(Some("Hello good sir!"))))
-
-    // convert back to json, and then to a pretty printed string, alternative
-    // ways to print may be nospaces, spaces2, or a custom format
-
-    val result = nice.asJson
-    println(result.spaces4)
-
-    assert(result.array.exists(_.length == 3))
-  }
-}
-
-
-case class Address(street: String, number: Int, postcode: Int)
-
-object Address {
-  // Define codecs easily from case classes
-  implicit def AddressCodecJson: CodecJson[Address] =
-    casecodec3(Address.apply, Address.unapply)("street", "number", "post_code")
-}
-
-case class Person(name: String, age: Int, address: Option[Address], greeting: Option[String])
-
-object Person {
-  implicit def PersonCodecJson: CodecJson[Person] =
-    casecodec4(Person.apply, Person.unapply)("name", "age", "address", "greeting")
 }
